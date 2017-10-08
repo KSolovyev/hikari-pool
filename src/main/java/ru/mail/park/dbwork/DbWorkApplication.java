283147125d3c53fe32d78ddac0832ebe8a604e0b
Solovyev Konstantin <k.solovyev@my.com>
2017-10-08 21:35:33 +0300

    concept proof: Hikari pool does work

---
+++ src/main/java/ru/mail/park/dbwork/DbWorkApplication.java
@@ -0,0 +1,12 @@
+package ru.mail.park.dbwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbWorkApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/mail/park/dbwork/DbWorkApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.mail.park.dbwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbWorkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
